0a0c6c5c412e60def0fd8ec649f6c6d146a32011
Roro21007 <rharris21007@gmail.com>
2023-11-25 18:09:57 -0600

    initialize project and npm

---
+++ index.js
@@ -0,0 +1,13 @@
+// prompt for text (3 characters)
+// prompt for text color (keyword or hexidecimal)
+// prompt for shape, presented with list (circle, triangle, square)
+// prompt for shape color (keyword or hexidecimal)
+// SVG file created named 'logo.svg' ('Generated logo.svg' is printed command line)
+// open 'logo.svg' file in browser shown 300x200 px that matches criteria
+
+// video submission
+// jest to test
+// inquirer 8.2.4
+
+const inquirer = require('inquirer');
+
